1099e551d5981a7f240a7158b60a0be6afdda584
Andy Ai <yanbo.ai@gmail.com>
2015-05-08 15:03:54 +0800

    Update posts.js

--- javascripts/posts.js
+++ javascripts/posts.js
@@ -1,12 +1,12 @@
 var posts = [
     {
         title: "Spark run start-all.sh using password-less ssh",
-        desc: "Spark 提供了独立版本的(不依赖任何其他工具)的集群方式，我们可以通过 start-all.sh 来启动一个 Spark 集群。 ...more",
+        desc: "Spark 提供了独立版本的(不依赖任何其他工具)的集群方式，我们可以通过 start-all.sh 来启动一个 Spark 集群。",
         link: "https://zybuluo.com/yanbo-ai/note/43894"
     },
     {
         title: "Spark 常规故障处理: Too many open files",
-        desc: "当你在 Linux 系统上使用 SparkContext.textFile 加载本地文件系统(文件目录)的数据时，可能会遇到下面的错误 ...more",
+        desc: "当你在 Linux 系统上使用 SparkContext.textFile 加载本地文件系统(文件目录)的数据时，可能会遇到下面的错误",
         link: "https://zybuluo.com/yanbo-ai/note/43455"
     },
     {
@@ -16,12 +16,12 @@ var posts = [
     },
     {
         title: "Databricks Spark Knowledge Base 简体中文",
-        desc: "翻译自 Databricks Spark Knowledge Base ...more",
+        desc: "翻译自 Databricks Spark Knowledge Base",
         link: "http://aiyanbo.gitbooks.io/databricks-spark-knowledge-base-zh-cn/"
     },
     {
         title: "RESTful Best Practices",
-        desc: "此文为实践总结，是自己在实践过程中积累的经验和哲学。部分内容参考相关资料，参考内容请看尾页。建议对RESTful有一定了解者阅读! ...more",
+        desc: "此文为实践总结，是自己在实践过程中积累的经验和哲学。部分内容参考相关资料，参考内容请看尾页。建议对RESTful有一定了解者阅读!",
         link: "https://zybuluo.com/yanbo-ai/note/17890"
     },
     {
